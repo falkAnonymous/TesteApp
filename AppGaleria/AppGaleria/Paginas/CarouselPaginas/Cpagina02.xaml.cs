36225cf367433a2954dc7694db10409bd7719196
falkAnonymous <davidmartinsantos77@gmail.com>
2023-08-19 15:36:18 -0300

    Criando um Carousel pelo Notebook, Separado do Master

---
+++ AppGaleria/AppGaleria/Paginas/CarouselPaginas/Cpagina02.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace AppGaleria.Paginas.CarouselPaginas
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class Cpagina02 : ContentPage
+	{
+		public Cpagina02 ()
+		{
+			InitializeComponent ();
+		}
+	}
+}
---
+++ AppGaleria/AppGaleria/Paginas/CarouselPaginas/PaginaPrincipal1.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace AppGaleria.Paginas.CarouselPaginas
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class PaginaPrincipal1 : ContentPage
+	{
+		public PaginaPrincipal1 ()
+		{
+			InitializeComponent ();
+		}
+	}
+}
