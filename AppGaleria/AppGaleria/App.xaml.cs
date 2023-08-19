ae7f540aff4c443a2058ad4aa937daae3db2e470
falkAnonymous <davidmartinsantos77@gmail.com>
2023-08-19 18:31:57 -0300

    Chamando as paginas do carrousel no Arquivo APP.cs usando a linguagem Csharp

--- AppGaleria/AppGaleria/App.xaml.cs
+++ AppGaleria/AppGaleria/App.xaml.cs
@@ -4,6 +4,8 @@ using Xamarin.Forms.Xaml;
 using AppGaleria.Paginas.Conteudos;
 using AppGaleria.Paginas.NavPaginas;
 using AppGaleria.Paginas.Modal;
+using AppGaleria.Paginas.CarouselPaginas;
+
 
 namespace AppGaleria
 {
@@ -12,8 +14,15 @@ namespace AppGaleria
         public App()
         {
             InitializeComponent();
-            object pagina = new NavigationPage(new Mpagina01());
-            MainPage = (Page)pagina;
+            //object pagina = new NavigationPage(new Mpagina01());
+
+            var paginas = new CarouselPage();
+            paginas.Children.Add(new PaginaPrincipal1());
+            paginas.Children.Add(new Cpagina02());
+            paginas.Children.Add(new Cpagina03());
+            paginas.CurrentPage = paginas.Children[1];
+
+            MainPage = paginas;
         }
 
         protected override void OnStart()
